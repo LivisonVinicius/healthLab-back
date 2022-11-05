3c5c9f2618926aa5f53e8f94b2542e3199c6d631
Livison Vinícius <livisongc@gmail.com>
2022-11-05 12:22:00 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import "express-async-errors";
 import router from "./routers/index";
-import errorHadler from "./middlewares/errorHandler";
+import errorHandler from "./middlewares/errorHandler";
 
 dotenv.config();
 
@@ -12,6 +12,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(router);
-app.use(errorHadler);
+app.use(errorHandler);
 
 export default app;
